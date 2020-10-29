30295131e0037cede24a502914b840e323b58303
Pedro Marques <pedromarques1011@gmail.com>
2020-10-28 21:49:41 -0300

    inicio ex2

--- bloco_10/ex1.js
+++ bloco_10/ex1.js
@@ -1,1 +1,50 @@
-ue;
+// testes-automatizados-com-jest.1
+
+const assert = require("assert");
+
+function sum(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("parameters must be numbers");
+  }
+
+  return a + b;
+}
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let i = 0, len = arr.length; i < len; i += 1) {
+    if (arr[i] === item) {
+      arr.splice(i, 1);
+      i -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+function myFizzBuzz(num) {
+  if (typeof num !== "number") return false;
+  if (num % 3 === 0 && num % 5 === 0) return "fizzbuzz";
+  if (num % 3 === 0) return "fizz";
+  if (num % 5 === 0) return "buzz";
+  return num;
+}
+
+module.exports = {
+  sum,
+  myRemove,
+  myRemoveWithoutCopy,
+  myFizzBuzz,
+};
+//exports.otherMethod = function() {};
+// implemente seus testes aqui

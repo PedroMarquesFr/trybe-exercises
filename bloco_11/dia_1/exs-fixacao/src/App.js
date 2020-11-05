11d127085c3aab07e90aee2f2f8f42178e5fb021
Pedro Marques <pedromarques1011@gmail.com>
2020-11-05 19:37:16 -0300

    exs1 fim

--- bloco_11/dia_1/exs-fixacao/src/App.js
+++ bloco_11/dia_1/exs-fixacao/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+const task = (value) => {
+  return <li>{value}</li>;
+};
+
+const compromissos = [
+  "ler",
+  "estudar",
+  "academia",
+  "escutar musica",
+  "aula sincrona 14:00",
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {compromissos.map((compromisso) => task(compromisso))}
     </div>
   );
 }

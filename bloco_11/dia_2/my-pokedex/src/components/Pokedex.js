d223ff14d3a3226ec1d04296ab153e3206818aac
Pedro Marques <pedromarques1011@gmail.com>
2020-11-06 19:24:46 -0300

    feito

---
+++ bloco_11/dia_2/my-pokedex/src/components/Pokedex.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Pokemon from "./Pokemon";
+import PropTypes from "prop-types";
+
+class Pokedex extends React.Component {
+  render() {
+    return (
+      <div className="pokedex">
+        {this.props.pokemons.map(
+          ({ name, type, averageWeight, image }, index) => {
+            return (
+              <Pokemon
+                key={index}
+                name={name}
+                type={type}
+                averageWeight={averageWeight}
+                image={image}
+              />
+            );
+          }
+        )}
+      </div>
+    );
+  }
+}
+
+Pokedex.propTypes = {
+  pokemons: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      type: PropTypes.string,
+      averageWeight: PropTypes.shape({
+        value: PropTypes.number,
+        measurementUnit: PropTypes.string,
+      }),
+      image: PropTypes.string,
+      moreInfo: PropTypes.string,
+    })
+  ),
+};
+
+export default Pokedex;

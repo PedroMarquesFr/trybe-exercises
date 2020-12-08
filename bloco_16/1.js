924bf237f133cfdb194d97f4103b928709d07980
Pedro Marques <pedromarques1011@gmail.com>
2020-12-08 18:21:39 -0300

    ex1

--- bloco_16/1.js
+++ bloco_16/1.js
@@ -1,4 +1,4 @@
-//Definindo um action creator
+// Definindo um action creator
 const action = {
     type: 'LOGIN'
   }
@@ -6,3 +6,5 @@ const action = {
   function actionCreator() {
     return action;
   }
+
+// After creating an action, the next step is sending the action to the Redux store so it can update its state. In Redux, you define action creators to accomplish this. An action creator is simply a JavaScript function that returns an action. In other words, action creators create objects that represent action events.

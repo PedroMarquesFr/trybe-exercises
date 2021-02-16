d3fd37ac5adfaaee0a234d5891b4594373d0af82
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-02-16 18:13:03 -0300

    ex index

---
+++ bloco_22/dia_3 transformando ideias em um modelo de banco de dados/exercicios/exINDEX.sql
@@ -0,0 +1,29 @@
+-- Verifique o impacto de um FULLTEXT INDEX na tabela category (banco de dados sakila ), adicionando-o na coluna name . Após ter adicionado o índice, mensure o custo da query utilizando o execution plan, como já foi feito em lições anteriores. Após ter criado e mensurado o custo da query, exclua o índice e mensure novamente esse custo.
+
+SELECT * FROM category;
+CREATE FULLTEXT INDEX lalal
+ON category(name);
+
+-- Após ter criado o índice, mensure o custo com a seguinte query:
+SELECT *
+FROM sakila.category
+WHERE MATCH(name) AGAINST('action');
+
+-- Após ter excluído o índice, mensure o custo com a seguinte query:
+-- DROP INDEX lalal ON category;
+SELECT *
+FROM sakila.category
+WHERE name LIKE '%action%';
+
+-- Verifique o impacto de um INDEX na tabela address (banco de dados sakila ) adicionando-o na coluna postal_code . Após ter adicionado o índice, mensure o custo da query utilizando o execution plan, como já foi feito em lições anteriores. Após ter criado e mensurado o custo da query, exclua o índice e mensure novamente esse custo.
+SELECT * FROM address;
+
+CREATE INDEX lol
+ON address(postal_code);
+
+-- Mensure o custo com a seguinte query:
+SELECT *
+FROM sakila.address
+WHERE postal_code = '36693';
+
+DROP INDEX lol ON address;

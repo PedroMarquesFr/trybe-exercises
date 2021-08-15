9c682ba17041b24e9fe0b0b614152663d1d70fc4
PedroMarquesFr2 <pedromarques1011@gmail.com>
2021-08-15 11:48:57 -0300

    corrigido estrutura classe deque

--- bloco_38/dia_2 deques ou filas/deques_em_python.py
+++ bloco_38/dia_2 deques ou filas/deques_em_python.py
@@ -2,7 +2,22 @@
 
 
 class Deque:
-    # ...
+    FIRST_ELEMENT = 0
+
+    def __init__(self):
+        self._data = []
+
+    def __len__(self):
+        return len(self._data)
+
+    def __str__(self):
+        return "Deque(" + ", ".join(str(x) for x in self._data) + ")"
+
+    def push_front(self, value):
+        self._data.insert(self.FIRST_ELEMENT, value)
+
+    def push_back(self, value):
+        self._data.append(value)
 
     def peek_front(self):
         if self._data:
@@ -12,6 +27,16 @@ class Deque:
     def peek_back(self):
         if self._data:
             return self._data[len(self) - 1]
+        return None
+
+    def pop_front(self):
+        if self._data:
+            return self._data.pop(self.FIRST_ELEMENT)
+        return None
+
+    def pop_back(self):
+        if self._data:
+            return self._data.pop()
         return None
 
 
